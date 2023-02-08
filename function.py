ffb272c5ba6e7158db74e3332fc1e2ddb279c1b2
Pankaj Shah <51921322+impankaj91@users.noreply.github.com>
2023-02-08 20:16:16 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+  const response = event.Records[0].cf.response;
+  const headers = response.headers;
+
+  headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubdomains; preload'}];
+  headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
+  headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}];
+  headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
+  headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}];
+
+  return response;
+};
